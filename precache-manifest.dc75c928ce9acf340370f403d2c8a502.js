d8073322b3a79601621f1a4f3f85308adb75c6f5
oskar <o.haller@isarsoft.com>
2020-09-26 17:57:34 +0200

    Updates

---
+++ precache-manifest.dc75c928ce9acf340370f403d2c8a502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330d6b39650ce90462b4932d3058b20c",
+    "url": "/mshack2020-frontend/index.html"
+  },
+  {
+    "revision": "cd12428e13eab5562fac",
+    "url": "/mshack2020-frontend/static/js/2.135f2026.chunk.js"
+  },
+  {
+    "revision": "bc49a19cc064f162762927cde2d92f51",
+    "url": "/mshack2020-frontend/static/js/2.135f2026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1bbdbf4e0ca59e3351",
+    "url": "/mshack2020-frontend/static/js/main.519f7266.chunk.js"
+  },
+  {
+    "revision": "3e366ed2d46129bf0cec",
+    "url": "/mshack2020-frontend/static/js/runtime-main.b27e76f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab64b194ed359ddf15739dc930a5d999.js"
+  "/mshack2020-frontend/precache-manifest.dc75c928ce9acf340370f403d2c8a502.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mshack2020-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
